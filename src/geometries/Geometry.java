68b3b25fd47f2e1170f71958bc79f5641bd8adae
Binyamin Mor <bmor@g.jct.ac.il>
2022-03-04 12:43:31 +0200

    Documentation

--- src/geometries/Geometry.java
+++ src/geometries/Geometry.java
@@ -3,6 +3,9 @@ package geometries;
 import primitives.Point;
 import primitives.Vector;
 
+/**
+ * Interface for all geometries with one func
+ */
 public interface Geometry {
     public Vector getNormal(Point p);
 }
