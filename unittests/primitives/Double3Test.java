5780841d0b22737f3d4992213b2165169a0600a1
tal rodgold <talrodgold@gmail.com>
2022-03-07 19:31:16 +0200

    added tests to double3 and point

--- unittests/primitives/Double3Test.java
+++ unittests/primitives/Double3Test.java
@@ -8,21 +8,41 @@ class Double3Test {
 
     @Test
     void add() {
+        Double3 d1 = new Double3(1,1,1);
+        Double3 d2 = new Double3(2,2,2);
+        Double3 d3 = new Double3(3,3,3);
+        assertEquals(d1.add(d2), d3, "Adding double3 to double3 Failed");
     }
 
     @Test
     void subtract() {
+        Double3 d1 = new Double3(1,1,1);
+        Double3 d2 = new Double3(2,2,2);
+        Double3 d3 = new Double3(3,3,3);
+        assertEquals(d3.subtract(d2), d1, "Subtracting double3 to double3 Failed");
     }
 
     @Test
     void scale() {
+        Double3 d1 = new Double3(1,1,1);
+        Double3 d2 = new Double3(2,2,2);
+        double num = 2;
+        assertEquals(d1.scale(num), d2, "Scaling double3 by a double Failed");
     }
 
     @Test
     void reduce() {
+        Double3 d1 = new Double3(1,1,1);
+        Double3 d2 = new Double3(2,2,2);
+        double num = 2;
+        assertEquals(d2.scale(num), d1, "Reducing double3 by a double Failed");
     }
 
     @Test
     void product() {
+        Double3 d1 = new Double3(4,4,4);
+        Double3 d2 = new Double3(2,2,2);
+        Double3 d3 = new Double3(8,8,8);
+        assertEquals(d1.product(d2), d3, "Product double3 by double3 Failed");
     }
 }
--- unittests/primitives/PointTest.java
+++ unittests/primitives/PointTest.java
@@ -10,20 +10,32 @@ class PointTest {
     void add() {
         Point p1 = new Point(1,1,1);
         Vector v1 = new Vector(2,2,2);
-        Point p3 = new Point(3,3,3);
-        assertEquals(p1.add(v1), p3, "Errorrrrrrrrrrrrrrrr");
+        Point p2 = new Point(3,3,3);
+        assertEquals(p1.add(v1), p2, "Add point to vector Failed");
     }
 
     @Test
     void subtract() {
-
+        Point p1 = new Point(1,1,1);
+        Point p2 = new Point(3,3,3);
+        Vector v1 = new Vector(2,2,2);
+        assertEquals(p2.subtract(p1), v1, "Subtract point from point Failed");
     }
 
     @Test
     void distanceSquared() {
+        Point p1 = new Point(1,1,1);
+        Point p2 = new Point(2,2,2);
+        double d1 = 3;
+        assertEquals(p2.distanceSquared(p1), d1, "Distance squared from point to point Failed");
+
     }
 
     @Test
     void distance() {
+        Point p1 = new Point(1,1,1);
+        Point p2 = new Point(2,2,2);
+        double d1 = 3;
+        assertEquals(p2.distanceSquared(p1), d1, "Distance from point to point Failed");
     }
 }
