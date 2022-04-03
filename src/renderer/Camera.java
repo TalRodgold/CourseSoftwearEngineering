6acd95075352f890115764ff818a25f6c3922dcd
Binyamin Mor <bmor@g.jct.ac.il>
2022-04-03 23:26:32 +0300

    Camera Class

--- src/renderer/Camera.java
+++ src/renderer/Camera.java
@@ -7,7 +7,7 @@ import static primitives.Util.alignZero;
 import static primitives.Util.isZero;
 
 public class Camera {
-    private Point p0;
+    private Point p0; // Camera's fields
     private Vector vTo;
     private Vector vUp;
     private Vector vRight;
@@ -15,19 +15,19 @@ public class Camera {
     private double height;
     private double distance;
 
-    public Camera(Point p01, Vector vTo1, Vector vUp1) {
-        if (vTo1.dotProduct(vUp1) != 0) {
+    public Camera(Point p01, Vector vTo1, Vector vUp1) { // C-tor for camera
+        if (vTo1.dotProduct(vUp1) != 0) { // if they are not orthogonal
         throw new IllegalArgumentException("Two vectors are not orthogonal");
         }
         this.p0 = p01;
-        this.vTo = vTo1.normalize();
+        this.vTo = vTo1.normalize(); // normalizing the vectors
         this.vUp = vUp1.normalize();
         this.vRight = vTo1.crossProduct(vUp1).normalize();
     }
 
     public Point getP0() {
         return p0;
-    }
+    }  // getters
 
     public Vector getvTo() {
         return vTo;
@@ -52,7 +52,7 @@ public class Camera {
     public double getDistance() {
         return distance;
     }
-    public Camera setVPSize(double width1, double height1){
+    public Camera setVPSize(double width1, double height1){  // setters
         this.width = width1;
         this.height = height1;
         return this;
@@ -62,27 +62,20 @@ public class Camera {
         return this;
     }
     public Ray constructRay(int nX, int nY, int j, int i) {
-        // calculate the ratio of the pixel by the height and by the width of the view plane
-        // the ratio Ry = h/Ny, the height of the pixel
-        double rY = alignZero(height / nY);
-        // the ratio Rx = w/Nx, the width of the pixel
-        double rX = alignZero(width / nX);
 
-        double xJ = alignZero((j - ((nX - 1d) / 2d)) * rX); // Xj = (j - (Nx -1)/2) * Rx
-        // Yi = -(i - (Ny - 1)/2) * Ry
+        double rY = alignZero(height / nY); //  ratio of height of pixel
+        double rX = alignZero(width / nX); // ratio of width of pixel
+        double xJ = alignZero((j - ((nX - 1d) / 2d)) * rX); // according to slideshow 4
         double yI = alignZero(-(i - ((nY - 1d) / 2d)) * rY);
 
         Point pIJ = p0.add(vTo.scale(distance));
-
-        if (!isZero(xJ)) {
+        if (!isZero(xJ)) {                 // if not 0 then scale and add
             pIJ = pIJ.add(vRight.scale(xJ));
         }
         if (!isZero(yI)) {
             pIJ = pIJ.add(vUp.scale(yI));
         }
-
-        //Vi,j = Pi,j - P0, the direction of the ray to the pixel(j, i)
-        Vector vIJ = pIJ.subtract(p0);
+        Vector vIJ = pIJ.subtract(p0); // direction of ray to pixel
         return new Ray(p0, vIJ);
     }
 }
