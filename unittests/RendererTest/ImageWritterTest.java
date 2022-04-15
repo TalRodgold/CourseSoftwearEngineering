4c6f0792f61b82cdea71218e512bbdfe5fa7663a
tal rodgold <talrodgold@gmail.com>
2022-04-15 12:22:57 +0300

    added write image test

--- unittests/RendererTest/ImageWritterTest.java
+++ unittests/RendererTest/ImageWritterTest.java
@@ -1,12 +1,26 @@
 package RendererTest;
 
 import org.junit.jupiter.api.Test;
+import primitives.Color;
+import renderer.ImageWriter;
 
 public class ImageWritterTest {
 
     @Test
     public  void testWriteImage(){
-        //ImageWritter i = new
-
+        ImageWriter imageWriter = new ImageWriter("test",800,500); // create 800x500 resolution
+        for (int i = 0; i < imageWriter.getNx(); i++) {
+            for (int j = 0; j < imageWriter.getNy(); j++) {
+                if (i % 50 == 0) { // every 50th pixel make a "grid" pixel
+                    imageWriter.writePixel(i, j, Color.RED);
+                }
+                else if (j % 50 == 0) {
+                    imageWriter.writePixel(i, j, Color.RED);
+                } else {
+                    imageWriter.writePixel(i, j, Color.YELLOW);
+                }
+            }
+        }
+        imageWriter.writeToImage();
     }
 }
